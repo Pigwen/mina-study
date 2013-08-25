af8b1b439c2228745bd37b61ced42bfae942aecd
Pigwen <sitereginfo@gmail.com>
2013-08-26 00:26:55 +0800

    add the TimeServer example

---
+++ src/main/java/org/maodian/mina/ch02/MinaTimeServer.java
@@ -0,0 +1,31 @@
+package org.maodian.mina.ch02;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+public class MinaTimeServer {
+  private static final int PORT = 9123;
+
+  /**
+   * @param args
+   * @throws IOException
+   */
+  public static void main(String[] args) throws IOException {
+    IoAcceptor acceptor = new NioSocketAcceptor();
+    acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+    acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(StandardCharsets.UTF_8)));
+    acceptor.setHandler(new TimeServerHandler());
+    acceptor.getSessionConfig().setReadBufferSize(2048);
+    acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+    acceptor.bind(new InetSocketAddress(PORT));
+  }
+
+}
---
+++ src/main/java/org/maodian/mina/ch02/TimeServerHandler.java
@@ -0,0 +1,29 @@
+package org.maodian.mina.ch02;
+
+import java.util.Date;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
+
+public class TimeServerHandler extends IoHandlerAdapter {
+  public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+    cause.printStackTrace();
+  }
+
+  public void messageReceived(IoSession session, Object message) throws Exception {
+    String str = message.toString();
+    if (str.trim().equalsIgnoreCase("quit")) {
+      session.close(true);
+      return;
+    }
+
+    Date now = new Date();
+    session.write(now.toString());
+    System.out.println("Message written...");
+  }
+
+  public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+    System.out.println("IDLE " + session.getIdleCount(status));
+  }
+}
